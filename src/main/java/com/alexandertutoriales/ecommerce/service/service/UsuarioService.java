ee7f8f0a79f3f22ea576dc261432d45dfb801111
wcachimbo <wcachimbo@procesosytecnologia.com>
2023-01-27 09:57:14 -0500

    nuevo proyecto

---
+++ src/main/java/com/alexandertutoriales/ecommerce/service/service/UsuarioService.java
@@ -0,0 +1,51 @@
+package com.alexandertutoriales.ecommerce.service.service;
+
+import com.alexandertutoriales.ecommerce.service.entity.Usuario;
+import com.alexandertutoriales.ecommerce.service.repository.UsuarioRepository;
+import com.alexandertutoriales.ecommerce.service.utlis.GenericResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import sun.util.locale.provider.HostLocaleProviderAdapter;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+import static com.alexandertutoriales.ecommerce.service.utlis.Global.*;
+
+@Service
+@Transactional
+@Slf4j
+public class UsuarioService {
+    private final UsuarioRepository repository;
+
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+    //Método para iniciar sesión
+    public GenericResponse<Usuario> login(String email, String contrasenia){
+        Optional<Usuario> optU = this.repository.login(email, contrasenia);
+
+        if(optU.isPresent()){
+            log.info("Inicio sesion correctamente");
+            log.info("Usuario:" + email);
+            log.info("contraseña:" + contrasenia);
+            return new GenericResponse<Usuario>(TIPO_AUTH, RPTA_OK, "Haz iniciado sesión correctamente", optU.get());
+
+        }else{
+            log.info("Revise su usuario y contraseña");
+            log.info("Usuario:" + email);
+            log.info("contraseña:" + contrasenia);
+            return new GenericResponse<Usuario>(TIPO_AUTH, RPTA_WARNING, "Lo sentimos, ese usuario no existe", new Usuario());
+        }
+    }
+    //Método para guardar credenciales del usuario
+    public GenericResponse guardarUsuario(Usuario u){
+        Optional<Usuario> optU = this.repository.findById(u.getId());
+        int idf = optU.isPresent() ? optU.get().getId() : 0;
+        if(idf == 0){
+            return new GenericResponse(TIPO_DATA, RPTA_OK, "Usuario Registrado Correctamente", this.repository.save(u));
+        }else{
+            return new GenericResponse(TIPO_DATA, RPTA_OK, "Datos del usuario actualizados", this.repository.save(u));
+        }
+    }
+}
